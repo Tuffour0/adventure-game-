17c0d1cdf536a629707378538393b2add2206eec
ozzy tuffour <osberttuffour14@gmail.com>
2026-04-19 15:14:23 -0400

    intial commit

---
+++ New Python Source File.py
@@ -0,0 +1,6 @@
+# Collect employee data using the following input() function:
+
+name = input("Enter you fullname :") 
+department = input("Enter department: ")
+experience = input("Enter experience: ")
+salary = input("Enter salary :")
